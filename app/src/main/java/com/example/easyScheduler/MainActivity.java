c12200bc84d73ce35a78598117b239785bef3f28
John Wandera <johnwandera37@gmail.com>
2021-11-14 16:26:32 +0300

    adding new home interface and java code, two xml files added preferences and help xml

--- app/src/main/java/com/example/easyScheduler/MainActivity.java
+++ app/src/main/java/com/example/easyScheduler/MainActivity.java
@@ -1,60 +1,70 @@
 package com.example.easyScheduler;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.app.Activity;
+import android.content.ClipData;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.bottomappbar.BottomAppBar;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
 public class MainActivity extends AppCompatActivity {
-    private Button button1;
-    private Button button2;
-    private Button button3;
-    private Button button4;
+    private BottomNavigationView bottomNavigationView;
+    private Toolbar topToolBar;
+    private BottomAppBar bottomAppBar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button1 = findViewById(R.id.AlarmActivity);
-        button2 = findViewById(R.id.MessageActivity);
-        button3 = findViewById(R.id.CallActivity);
-        button4 = findViewById(R.id.LinkActivity);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setBackground(null);
+
+        topToolBar = findViewById(R.id.topAppBar);
+        setSupportActionBar(topToolBar);
+
+        bottomAppBar = findViewById(R.id.bottomAppBar);
+        setSupportActionBar(bottomAppBar);
 
 
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this, AlarmActivity.class);
-                startActivity(i);
-            }
-        });
-
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this, MessageActivity.class);
-                startActivity(i);
-            }
-        });
-
-        button3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this, CallActivity.class);
-                startActivity(i);
-            }
-        });
 
 
-        button4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this, LinkActivity.class);
-                startActivity(i);
-            }
-        });
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.top_menu_bar,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.preferences){
+            Intent intent =  new Intent(MainActivity.this,Preferences.class);
+            startActivity(intent);
+            return true;
+        }
+        else if(id == R.id.help) {
+            Intent intent1 = new Intent(MainActivity.this, Help.class);
+            startActivity(intent1);
+            return true;
+        }
+            return super.onOptionsItemSelected(item);
+
+    }
+
+
 }
